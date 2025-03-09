33c8759001fb9d2fb225c50769c9990fc7e1d384
pwc2002 <pwc2002@naver.com>
2025-03-09 16:35:26 +0900

    Feat: 고정리더보드 기초 틀 구현

--- components/fixed-leaderboard.jsx
+++ components/fixed-leaderboard.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 
 export default function FixedLeaderboard() {
     return (
-        <div>
-            
+        <div className="fixed top-0 right-0 w-1/4 h-full bg-white border-1 text-black flex flex-col items-center">
+            <div className="flex flex-row justify-center">
+                asdfasd
+            </div>
         </div>
     );
 }
