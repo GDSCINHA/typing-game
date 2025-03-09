2212334038361e59e89091e8757779771e247e6e
pwc2002 <pwc2002@naver.com>
2025-03-09 14:24:18 +0900

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+import { Button } from "@heroui/button";
+
+
+export default function Home() {
+  return (
+   <div>
+    <div>
+      <h1>Hello World</h1>
+      <Button className="bg-red-500">Click me</Button>
+    </div>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+    
+   </div>
+  );
+}
